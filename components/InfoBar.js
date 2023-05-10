be2dd0192dd91c9b0d05af0e0e2f6aa8c0e0ae2e
Florian Schneider <schneider.f@me.com>
2023-05-10 22:09:32 +0200

    infobar update

--- components/InfoBar.js
+++ components/InfoBar.js
@@ -1,18 +1,39 @@
 // components/InfoBar.js
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/InfoBar.module.css";
+import { MdAccessTime, MdPhone } from "react-icons/md"; // import the icons
+import { useMediaQuery } from "react-responsive"; // import the hook
 
 const InfoBar = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const isDesktopOrLaptop = useMediaQuery({ minWidth: 1024 }); // define the media query
+
   return (
     <div className={styles.infoBar}>
       <div className={styles.openingHours}>
-        Öffnungszeiten:
-        <span className={styles.mobileBreak}>
-          <br />
-        </span>
-        Mo-Do 08:30-11:00 Uhr | Fr & Sa 09:30-10:30 Uhr | So: Geschlossen
+        {isClient &&
+          (isDesktopOrLaptop ? (
+            "Öffnungszeiten: "
+          ) : (
+            <MdAccessTime size={20} style={{ verticalAlign: "middle" }} />
+          ))}
+        <span> Mo-Do 08:30-11:00 Uhr | Fr & Sa 09:30-10:30 Uhr</span>
+        <span className={styles.sundayHours}> | So: Geschlossen</span>
       </div>
-      <div className={styles.phoneNumber}>Telefon: +49 (0) 491 9293713</div>
+      <div className={styles.phoneNumber}>
+        {isClient &&
+          (isDesktopOrLaptop ? (
+            "Telefon: "
+          ) : (
+            <MdPhone size={20} style={{ verticalAlign: "middle" }} />
+          ))}
+        <span> +49 (0) 491 9293713</span>
+      </div>
     </div>
   );
 };
