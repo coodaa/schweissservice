cae4a6d68a7fd4a51b1d2c54559a953bc4f71b77
Florian Schneider <schneider.f@me.com>
2023-05-12 01:06:35 +0200

    seo

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,41 @@
-import Layout from "../components/Layout";
+import React from "react";
+import Head from "next/head";
 import styles from "../styles/Index.module.css";
 import Image from "next/image";
 
 export default function Index() {
   return (
-    <Layout>
+    <>
+      <Head>
+        <title>WAGEMANN SCHWEISS-SERVICE</title>
+        <meta
+          name="description"
+          content="Beschreibung von WAGEMANN SCHWEISS-SERVICE"
+        />
+        <meta
+          name="keywords"
+          content="Schweißen, technische Gase, Leer, Ostfriesland, Wagemann"
+        />
+      </Head>
+
       <div className={styles.main}>
         <div className={styles.redSquare}>
           <Image
-            src="/assets/img/logo/logo-original.png"
+            src="/assets/img/logo/logo-full-white.png"
             alt="WAGEMANN SCHWEISS-SERVICE"
-            width={200}
-            height={80}
+            width={350}
+            height={150}
           />
-          <h1>Hello World</h1>
+          <h2>
+            Ihr kompetenter Partner für technische Gase und Schweißlösungen im
+            Herzen Ostfrieslands.
+          </h2>
         </div>
         <div
           className={styles.backgroundImage}
           alt="WAGEMANN SCHWEISS-SERVICE"
         />
       </div>
-    </Layout>
+    </>
   );
 }
