b4c6613d56476eac97e3763b6e699c335629466a
Florian Schneider <schneider.f@me.com>
2023-05-11 22:08:28 +0200

    delete

---
+++ components/Navbar1.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import styles from "../styles/InfoBar.module.css";
+import { MdAccessTime, MdPhone } from "react-icons/md"; // import the icons
+import { useMediaQuery } from "react-responsive"; // import the hook
+
+const InfoBar = () => {
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const isDesktopOrLaptop = useMediaQuery({ minWidth: 1024 }); //
+
+  return (
+    <div className={styles.infoBar}>
+      <div className={styles.openingHours}>
+        {isClient && (
+          <>
+            {isDesktopOrLaptop ? (
+              <span className={styles.textBold}>Öffnungszeiten: &nbsp;</span>
+            ) : (
+              <MdAccessTime
+                className={styles.icon}
+                size={20}
+                style={{ verticalAlign: "middle" }}
+              />
+            )}
+            <span>Mo-Do 08:30-11:00 Uhr | Fr & Sa 09:30-10:30 Uhr &nbsp;</span>
+            <span className={styles.sundayHours}>| So: Geschlossen</span>
+          </>
+        )}
+      </div>
+      <div className={styles.phoneNumber}>
+        {isClient && (
+          <>
+            {isDesktopOrLaptop ? (
+              <span className={styles.textBold}>Telefon: &nbsp;</span>
+            ) : (
+              <MdPhone
+                className={styles.icon}
+                size={20}
+                style={{ verticalAlign: "middle" }}
+              />
+            )}
+            <span>+49 (0) 491 9293713</span>
+          </>
+        )}
+      </div>{" "}
+    </div>
+  );
+};
+
+export default InfoBar;
