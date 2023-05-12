20ac431efc150c9ebc8a6664a677df0a395a50f4
Florian Schneider <schneider.f@me.com>
2023-05-12 14:25:06 +0200

    burger

--- components/InfoBar.js
+++ components/InfoBar.js
@@ -1,8 +1,88 @@
+// import React, { useState, useEffect } from "react";
+// import styles from "../styles/InfoBARR.module.css";
+// import { useMediaQuery } from "react-responsive";
+// import Image from "next/image";
+// import Link from "next/link";
+// import BurgerMenu from "./BurgerMenu";
+
+// const Infobar = ({ scrollPosition }) => {
+//   const [isClient, setIsClient] = useState(false);
+//   useEffect(() => {
+//     setIsClient(true);
+//   }, []);
+
+//   const [menuOpen, setMenuOpen] = useState(false);
+
+//   const isDesktopOrLaptop = useMediaQuery({ minWidth: 1024 });
+
+//   const handleToggle = (open) => {
+//     setMenuOpen(open);
+//   };
+
+//   return (
+//     <div className={styles.nav}>
+//       <div className={styles.logo}>
+//         <Image
+//           src="/assets/img/logo/logo-original.png"
+//           alt="WAGEMANN SCHWEISS-SERVICE"
+//           width={170}
+//           height={65}
+//         />
+//       </div>
+//       <div className={styles.phoneNumber}>
+//         <BurgerMenu onToggle={handleToggle} />
+//         {menuOpen && (
+//           <ul
+//             className={`${styles.navLinksMobile} ${
+//               menuOpen ? styles.show : ""
+//             }`}
+//           >
+//             <li>
+//               <Link href="/" passHref className={styles.link}>
+//                 Service
+//               </Link>
+//             </li>
+//             <li>
+//               <Link href="/geschichte" passHref className={styles.link}>
+//                 Geschichte
+//               </Link>
+//             </li>
+//             <li>
+//               <Link href="/kontakt" passHref className={styles.link}>
+//                 Kontakt
+//               </Link>
+//             </li>
+//           </ul>
+//         )}
+//         <ul className={styles.navLinksDesktop}>
+//           <li>
+//             <Link href="/" passHref className={styles.link}>
+//               Service
+//             </Link>
+//           </li>
+//           <li>
+//             <Link href="/geschichte" passHref className={styles.link}>
+//               Geschichte
+//             </Link>
+//           </li>
+//           <li>
+//             <Link href="/kontakt" passHref className={styles.link}>
+//               Kontakt
+//             </Link>
+//           </li>
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Infobar;
+
 import React, { useState, useEffect } from "react";
 import styles from "../styles/InfoBARR.module.css";
 import { useMediaQuery } from "react-responsive";
 import Image from "next/image";
-import Link from "next/link";
+import { Link as ScrollLink } from "react-scroll";
 import BurgerMenu from "./BurgerMenu";
 
 const Infobar = ({ scrollPosition }) => {
@@ -18,7 +98,6 @@ const Infobar = ({ scrollPosition }) => {
   const handleToggle = (open) => {
     setMenuOpen(open);
   };
-
   return (
     <div className={styles.nav}>
       <div className={styles.logo}>
@@ -30,6 +109,7 @@ const Infobar = ({ scrollPosition }) => {
         />
       </div>
       <div className={styles.phoneNumber}>
+        {" "}
         <BurgerMenu onToggle={handleToggle} />
         {menuOpen && (
           <ul
@@ -38,37 +118,67 @@ const Infobar = ({ scrollPosition }) => {
             }`}
           >
             <li>
-              <Link href="/" passHref className={styles.link}>
+              <ScrollLink
+                to="serviceSection"
+                smooth={true}
+                duration={500}
+                className={styles.link}
+              >
                 Service
-              </Link>
+              </ScrollLink>
             </li>
             <li>
-              <Link href="/geschichte" passHref className={styles.link}>
+              <ScrollLink
+                to="historySection"
+                smooth={true}
+                duration={500}
+                className={styles.link}
+              >
                 Geschichte
-              </Link>
+              </ScrollLink>
             </li>
             <li>
-              <Link href="/kontakt" passHref className={styles.link}>
+              <ScrollLink
+                to="contactSection"
+                smooth={true}
+                duration={500}
+                className={styles.link}
+              >
                 Kontakt
-              </Link>
+              </ScrollLink>
             </li>
           </ul>
         )}
         <ul className={styles.navLinksDesktop}>
           <li>
-            <Link href="/" passHref className={styles.link}>
+            <ScrollLink
+              to="serviceSection"
+              smooth={true}
+              duration={500}
+              className={styles.link}
+            >
               Service
-            </Link>
+            </ScrollLink>
           </li>
           <li>
-            <Link href="/geschichte" passHref className={styles.link}>
+            <ScrollLink
+              to="historySection"
+              smooth={true}
+              duration={500}
+              className={styles.link}
+            >
               Geschichte
-            </Link>
+            </ScrollLink>
           </li>
           <li>
-            <Link href="/kontakt" passHref className={styles.link}>
+            <ScrollLink
+              to="contactSection"
+              smooth={true}
+              duration={500}
+              className={styles.link}
+            >
               Kontakt
-            </Link>
+            </ScrollLink>
           </li>
         </ul>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -17,25 +17,49 @@ export default function Index() {
           content="Schweißen, technische Gase, Leer, Ostfriesland, Wagemann"
         />
       </Head>
-
-      <div className={styles.main}>
-        <div className={styles.redSquare}>
-          <Image
-            src="/assets/img/logo/logo-full-white.png"
+      <div id="serviceSection">
+        <div className={styles.main}>
+          <div className={styles.redSquare}>
+            <Image
+              src="/assets/img/logo/logo-full-white.png"
+              alt="WAGEMANN SCHWEISS-SERVICE"
+              width={350}
+              height={150}
+            />
+            <h2>
+              Ihr kompetenter Partner für technische Gase und Schweißlösungen im
+              Herzen Ostfrieslands.
+            </h2>
+          </div>
+          <div
+            className={styles.backgroundImage}
             alt="WAGEMANN SCHWEISS-SERVICE"
-            width={350}
-            height={150}
           />
-          <h2>
-            Ihr kompetenter Partner für technische Gase und Schweißlösungen im
-            Herzen Ostfrieslands.
-          </h2>
         </div>
-        <div
-          className={styles.backgroundImage}
-          alt="WAGEMANN SCHWEISS-SERVICE"
-        />
       </div>
+      <div className={styles.main} id="historySection">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+        ipsum dolor sit amet.
+      </div>
+      <div className={styles.main} id="contactSection">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+        ipsum dolor sit amet.
+      </div>
+      ;
     </>
   );
 }
