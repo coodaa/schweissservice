faff7a08ccbe4371375ff9837023d96383801d17
Florian Schneider <schneider.f@me.com>
2023-05-27 17:40:53 +0200

    mobile update

--- components/BurgerMenu.js
+++ components/BurgerMenu.js
@@ -2,16 +2,20 @@
 import React, { useState } from "react";
 import styles from "../styles/BurgerMenu.module.css";
 
-export default function BurgerMenu({ onToggle }) {
+export default function BurgerMenu({ onToggle, isOpen }) {
   const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-    onToggle(!menuOpen);
+    const newState = !menuOpen;
+    setMenuOpen(newState);
+    onToggle(newState);
   };
 
   return (
-    <div className={styles.burgerMenu} onClick={toggleMenu}>
+    <div
+      className={`${styles.burgerMenu} ${menuOpen ? styles.fullscreen : ""}`}
+      onClick={toggleMenu}
+    >
       <div className={`${styles.bar} ${menuOpen ? styles.open : ""}`}></div>
       <div className={`${styles.bar} ${menuOpen ? styles.open : ""}`}></div>
       <div className={`${styles.bar} ${menuOpen ? styles.open : ""}`}></div>
--- components/InfoBar.js
+++ components/InfoBar.js
@@ -6,18 +6,22 @@ import { Link as ScrollLink } from "react-scroll";
 import BurgerMenu from "./BurgerMenu";
 
 const Infobar = ({ scrollPosition }) => {
-  const [isClient, setIsClient] = useState(false);
+  const [isDesktopOrLaptop, setIsDesktopOrLaptop] = useState(false);
+
   useEffect(() => {
-    setIsClient(true);
+    if (window.innerWidth >= 1024) {
+      setIsDesktopOrLaptop(true);
+    } else {
+      setIsDesktopOrLaptop(false);
+    }
   }, []);
 
   const [menuOpen, setMenuOpen] = useState(false);
 
-  const isDesktopOrLaptop = useMediaQuery({ minWidth: 1024 });
-
   const handleToggle = (open) => {
     setMenuOpen(open);
   };
+
   return (
     <div className={styles.nav}>
       <ScrollLink
@@ -38,17 +42,10 @@ const Infobar = ({ scrollPosition }) => {
       </ScrollLink>
 
       <div className={styles.phoneNumber}>
-        {" "}
-        <BurgerMenu onToggle={handleToggle} />
-        {menuOpen && (
-          <ul
-            className={`${styles.navLinksMobile} ${
-              menuOpen ? styles.show : ""
-            }`}
-          >
+        {isDesktopOrLaptop ? (
+          <ul className={styles.navLinksDesktop}>
             <li>
               <ScrollLink
-                onClick={() => setMenuOpen(false)}
                 to="serviceSection"
                 smooth={true}
                 duration={500}
@@ -60,7 +57,6 @@ const Infobar = ({ scrollPosition }) => {
             </li>
             <li>
               <ScrollLink
-                onClick={() => setMenuOpen(false)}
                 to="historySection"
                 smooth={true}
                 duration={500}
@@ -72,7 +68,6 @@ const Infobar = ({ scrollPosition }) => {
             </li>
             <li>
               <ScrollLink
-                onClick={() => setMenuOpen(false)}
                 to="contactSection"
                 smooth={true}
                 duration={500}
@@ -83,42 +78,55 @@ const Infobar = ({ scrollPosition }) => {
               </ScrollLink>
             </li>
           </ul>
+        ) : (
+          <>
+            <BurgerMenu onToggle={handleToggle} isOpen={menuOpen} />
+            {menuOpen && (
+              <ul
+                className={`${styles.navLinksMobile} ${
+                  menuOpen ? styles.show : ""
+                }`}
+              >
+                <li>
+                  <ScrollLink
+                    onClick={() => setMenuOpen(false)}
+                    to="serviceSection"
+                    smooth={true}
+                    duration={500}
+                    offset={-60}
+                    className={styles.link}
+                  >
+                    Service
+                  </ScrollLink>
+                </li>
+                <li>
+                  <ScrollLink
+                    onClick={() => setMenuOpen(false)}
+                    to="historySection"
+                    smooth={true}
+                    duration={500}
+                    offset={-60}
+                    className={styles.link}
+                  >
+                    Geschichte
+                  </ScrollLink>
+                </li>
+                <li>
+                  <ScrollLink
+                    onClick={() => setMenuOpen(false)}
+                    to="contactSection"
+                    smooth={true}
+                    duration={500}
+                    offset={-60}
+                    className={styles.link}
+                  >
+                    Kontakt
+                  </ScrollLink>
+                </li>
+              </ul>
+            )}
+          </>
         )}
-        <ul className={styles.navLinksDesktop}>
-          <li>
-            <ScrollLink
-              to="serviceSection"
-              smooth={true}
-              duration={500}
-              offset={-60}
-              className={styles.link}
-            >
-              Service
-            </ScrollLink>
-          </li>
-          <li>
-            <ScrollLink
-              to="historySection"
-              smooth={true}
-              duration={500}
-              offset={-60}
-              className={styles.link}
-            >
-              Geschichte
-            </ScrollLink>
-          </li>
-          <li>
-            <ScrollLink
-              to="contactSection"
-              smooth={true}
-              duration={500}
-              offset={-60}
-              className={styles.link}
-            >
-              Kontakt
-            </ScrollLink>
-          </li>
-        </ul>
       </div>
     </div>
   );
