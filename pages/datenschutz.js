ec9859c0390f58307ac8b8e356817a8e4db16e0b
Florian Schneider <schneider.f@me.com>
2023-06-01 12:19:18 +0200

    seo impressum and datenschutz

--- pages/datenschutz.js
+++ pages/datenschutz.js
@@ -1,77 +1,97 @@
 import React from "react";
+import Head from "next/head";
 import styles from "../styles/Privacy.module.css";
 
 const Privacy = () => {
   return (
-    <div>
-      <div className={styles.container}>
-        <h1 className={styles.title}>Datenschutzerklärung</h1>
-        <h2 className={styles.subTitle}>1. Datenschutz auf einen Blick</h2>
-        <p className={styles.paragraph}>
-          Allgemeine Hinweise. Die folgenden Hinweise geben einen einfachen
-          Überblick darüber, was mit Ihren personenbezogenen Daten passiert,
-          wenn Sie unsere Website besuchen. Personenbezogene Daten sind alle
-          Daten, mit denen Sie persönlich identifiziert werden können.
-          Ausführliche Informationen zum Thema Datenschutz entnehmen Sie unserer
-          unter diesem Text aufgeführten Datenschutzerklärung.
-        </p>
-        <h2 className={styles.subTitle}>
-          2. Datenerfassung auf unserer Website
-        </h2>
-        <p className={styles.paragraph}>
-          <h3>
-            Wer ist verantwortlich für die Datenerfassung auf dieser Website?
-          </h3>
-          <p>
-            Die Datenverarbeitung auf dieser Website erfolgt durch den
-            Websitebetreiber. Dessen Kontaktdaten können Sie dem Impressum
-            dieser Website entnehmen.
+    <>
+      <Head>
+        <title>Datenschutzerklärung - Wagemann Schweiß-Service</title>
+        <meta
+          name="description"
+          content="Datenschutzerklärung der Website. Erfahren Sie, wie wir Daten erheben und verarbeiten und welche Rechte Sie in Bezug auf Ihre Daten haben."
+        />
+        <meta
+          property="og:title"
+          content="Datenschutzerklärung - Wagemann Schweiß-Service"
+        />
+        <meta
+          property="og:description"
+          content="Datenschutzerklärung der Website. Erfahren Sie, wie wir Daten erheben und verarbeiten und welche Rechte Sie in Bezug auf Ihre Daten haben."
+        />
+        <meta property="og:type" content="website" />
+      </Head>
+      <div>
+        <div className={styles.container}>
+          <h1 className={styles.title}>Datenschutzerklärung</h1>
+          <h2 className={styles.subTitle}>1. Datenschutz auf einen Blick</h2>
+          <p className={styles.paragraph}>
+            Allgemeine Hinweise. Die folgenden Hinweise geben einen einfachen
+            Überblick darüber, was mit Ihren personenbezogenen Daten passiert,
+            wenn Sie unsere Website besuchen. Personenbezogene Daten sind alle
+            Daten, mit denen Sie persönlich identifiziert werden können.
+            Ausführliche Informationen zum Thema Datenschutz entnehmen Sie
+            unserer unter diesem Text aufgeführten Datenschutzerklärung.
+          </p>
+          <h2 className={styles.subTitle}>
+            2. Datenerfassung auf unserer Website
+          </h2>
+          <p className={styles.paragraph}>
+            <h3>
+              Wer ist verantwortlich für die Datenerfassung auf dieser Website?
+            </h3>
+            <p>
+              Die Datenverarbeitung auf dieser Website erfolgt durch den
+              Websitebetreiber. Dessen Kontaktdaten können Sie dem Impressum
+              dieser Website entnehmen.
+            </p>
+
+            <h3>Wie erfassen wir Ihre Daten?</h3>
+            <p>
+              Ihre Daten werden zum einen dadurch erhoben, dass Sie uns diese
+              mitteilen. Hierbei kann es sich z.B. um Daten handeln, die Sie in
+              ein Kontaktformular eingeben.
+            </p>
+
+            <h3>Wofür nutzen wir Ihre Daten?</h3>
+            <p>
+              Ein Teil der Daten wird erhoben, um eine fehlerfreie
+              Bereitstellung der Website zu gewährleisten. Andere Daten können
+              zur Analyse Ihres Nutzerverhaltens verwendet werden.
+            </p>
+          </p>
+          <h2 className={styles.subTitle}>3. Ihre Rechte</h2>
+
+          <p className={styles.paragraph}>
+            <h3>Welche Rechte haben Sie bezüglich Ihrer Daten?</h3>
+            <p>
+              Sie haben jederzeit das Recht unentgeltlich Auskunft über
+              Herkunft, Empfänger und Zweck Ihrer gespeicherten
+              personenbezogenen Daten zu erhalten. Sie haben außerdem ein Recht,
+              die Berichtigung, Sperrung oder Löschung dieser Daten zu
+              verlangen. Hierzu sowie zu weiteren Fragen zum Thema Datenschutz
+              können Sie sich jederzeit unter der im Impressum angegebenen
+              Adresse an uns wenden. Des Weiteren steht Ihnen ein
+              Beschwerderecht bei der zuständigen Aufsichtsbehörde zu.
+            </p>
           </p>
 
-          <h3>Wie erfassen wir Ihre Daten?</h3>
+          <h2 className={styles.subTitle}>
+            4. Haftungsbeschränkung für externe Links
+          </h2>
           <p>
-            Ihre Daten werden zum einen dadurch erhoben, dass Sie uns diese
-            mitteilen. Hierbei kann es sich z.B. um Daten handeln, die Sie in
-            ein Kontaktformular eingeben.
+            Unsere Webseite enthält Verknüpfungen zu Webseiten Dritter (sog.
+            „externe Links“). Da wir auf deren Inhalte keinen Einfluss haben,
+            kann für die fremden Inhalte keine Gewähr übernommen werden. Für die
+            Inhalte und Richtigkeit der Informationen ist stets der jeweilige
+            Informationsanbieter der verlinkten Webseite verantwortlich. Zum
+            Zeitpunkt der Verlinkung waren keine Rechtsverstöße erkennbar.
+            Sobald uns eine Rechtsverletzung bekannt wird, werden wir den
+            jeweiligen Link umgehend entfernen.
           </p>
-
-          <h3>Wofür nutzen wir Ihre Daten?</h3>
-          <p>
-            Ein Teil der Daten wird erhoben, um eine fehlerfreie Bereitstellung
-            der Website zu gewährleisten. Andere Daten können zur Analyse Ihres
-            Nutzerverhaltens verwendet werden.
-          </p>
-        </p>
-        <h2 className={styles.subTitle}>3. Ihre Rechte</h2>
-
-        <p className={styles.paragraph}>
-          <h3>Welche Rechte haben Sie bezüglich Ihrer Daten?</h3>
-          <p>
-            Sie haben jederzeit das Recht unentgeltlich Auskunft über Herkunft,
-            Empfänger und Zweck Ihrer gespeicherten personenbezogenen Daten zu
-            erhalten. Sie haben außerdem ein Recht, die Berichtigung, Sperrung
-            oder Löschung dieser Daten zu verlangen. Hierzu sowie zu weiteren
-            Fragen zum Thema Datenschutz können Sie sich jederzeit unter der im
-            Impressum angegebenen Adresse an uns wenden. Des Weiteren steht
-            Ihnen ein Beschwerderecht bei der zuständigen Aufsichtsbehörde zu.
-          </p>
-        </p>
-
-        <h2 className={styles.subTitle}>
-          4. Haftungsbeschränkung für externe Links
-        </h2>
-        <p>
-          Unsere Webseite enthält Verknüpfungen zu Webseiten Dritter (sog.
-          „externe Links“). Da wir auf deren Inhalte keinen Einfluss haben, kann
-          für die fremden Inhalte keine Gewähr übernommen werden. Für die
-          Inhalte und Richtigkeit der Informationen ist stets der jeweilige
-          Informationsanbieter der verlinkten Webseite verantwortlich. Zum
-          Zeitpunkt der Verlinkung waren keine Rechtsverstöße erkennbar. Sobald
-          uns eine Rechtsverletzung bekannt wird, werden wir den jeweiligen Link
-          umgehend entfernen.
-        </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- pages/impressum.js
+++ pages/impressum.js
@@ -1,134 +1,137 @@
 import React from "react";
+import Head from "next/head";
 import styles from "../styles/Impressum.module.css";
 import { MdPhone, MdEmail } from "react-icons/md";
 
 const Impressum = () => {
   return (
-    <div>
-      <div className={styles.container}>
-        <h1 className={styles.title}>Impressum</h1>
-        <h2 className={styles.subtitle}>Angaben gemäß § 5 TMG:</h2>
-
-        <p className={styles.paragraph}>
-          Wagemann Schweiß Service UG <br />
-          (haftungsbeschränkt) <br />
-          Sägemühlenstraße 89
-          <br />
-          26789 Leer (Ostfriesland)
-        </p>
-
-        <h2 className={styles.subtitle}>Vertreten durch:</h2>
-
-        <p className={styles.paragraph}>Geschäftsführer Helga Wagemann</p>
-
-        <h2 className={styles.subtitle}>Kontakt:</h2>
-
-        <p>
-          <MdPhone
-            className={styles.link}
-            size={20}
-            style={{ verticalAlign: "middle" }}
-          />
-          &nbsp;
-          <a href="tel:+4904919293713">+49 (0) 491 9293713</a>
-        </p>
-        <p>
-          <MdEmail
-            className={styles.icon}
-            size={20}
-            style={{ verticalAlign: "middle" }}
-          />
-          &nbsp;
-          <a href="mailto:kontakt@wagemann-schweiss-service.de">
-            kontakt@wagemann-schweiss-service.de
-          </a>
-        </p>
-
-        <h2 className={styles.subtitle}>Registereintrag:</h2>
-
-        <p className={styles.paragraph}>
-          Eintragung im Handelsregister.
-          <br />
-          Registergericht: Amtsgericht Aurich
-          <br />
-          Registernummer: HRB 204232
-        </p>
-
-        {/* <h2 className={styles.subtitle}>Umsatzsteuer:</h2> */}
-
-        <p className={styles.paragraph}>
-          Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz:
-          <br />
-          DE 123456789
-        </p>
-
-        <h2 className={styles.subtitle}>
-          Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:
-        </h2>
-
-        <p className={styles.paragraph}>
-          Helga Wagemann <br />
-          Sägemühlenstraße 89
-          <br />
-          26789 Leer (Ostfriesland)
-        </p>
-
-        <h2 className={styles.subtitle}>Streitschlichtung</h2>
-
-        <p className={styles.paragraph}>
-          Die Europäische Kommission stellt eine Plattform zur
-          Online-Streitbeilegung (OS) bereit:{" "}
-          <a
-            href="https://ec.europa.eu/consumers/odr"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            https://ec.europa.eu/consumers/odr
-          </a>
-          .<br />
-          Unsere E-Mail-Adresse finden Sie oben im Impressum.
-          <br />
-          Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren
-          vor einer Verbraucherschlichtungsstelle teilzunehmen.
-        </p>
-
-        <h3 className={styles.subtitle}>Haftung für Inhalte</h3>
-
-        <p className={styles.paragraph}>
-          Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte
-          auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach
-          §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht
-          verpflichtet, übermittelte oder gespeicherte fremde Informationen zu
-          überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige
-          Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der
-          Nutzung von Informationen nach den allgemeinen Gesetzen bleiben
-          hiervon unberührt.
-        </p>
-
-        <h3 className={styles.subtitle}>Haftung für Links</h3>
-
-        <p className={styles.paragraph}>
-          Unser Angebot enthält Links zu externen Websites Dritter, auf deren
-          Inhalte wir keinen Einfluss haben. Deshalb können wir für diese
-          fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der
-          verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der
-          Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der
-          Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige
-          Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.
-        </p>
-
-        <h3 className={styles.subtitle}>Urheberrecht</h3>
-
-        <p className={styles.paragraph}>
-          Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen
-          Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung,
-          Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der
-          Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des
-          jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite
-          sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
-        </p>
+    <>
+      <Head>
+        <title>Impressum - Wagemann Schweiß Service UG</title>
+        <meta
+          name="description"
+          content="Impressum der Wagemann Schweiß Service UG. Angaben gemäß § 5 TMG, Kontaktinformationen, Registereintrag, Umsatzsteuer-Identifikationsnummer und weitere rechtliche Informationen."
+        />
+        <meta
+          property="og:title"
+          content="Impressum - Wagemann Schweiß Service UG"
+        />
+        <meta
+          property="og:description"
+          content="Impressum der Wagemann Schweiß Service UG. Angaben gemäß § 5 TMG, Kontaktinformationen, Registereintrag, Umsatzsteuer-Identifikationsnummer und weitere rechtliche Informationen."
+        />
+        <meta property="og:type" content="website" />
+      </Head>
+      <div>
+        <div className={styles.container}>
+          <h1 className={styles.title}>Impressum</h1>
+          <h2 className={styles.subtitle}>Angaben gemäß § 5 TMG:</h2>
+          <p className={styles.paragraph}>
+            Wagemann Schweiß Service UG <br />
+            (haftungsbeschränkt) <br />
+            Sägemühlenstraße 89
+            <br />
+            26789 Leer (Ostfriesland)
+          </p>
+          <h2 className={styles.subtitle}>Vertreten durch:</h2>
+          <p className={styles.paragraph}>Geschäftsführer Helga Wagemann</p>
+          <h2 className={styles.subtitle}>Kontakt:</h2>
+          <p>
+            <MdPhone
+              className={styles.link}
+              size={20}
+              style={{ verticalAlign: "middle" }}
+            />
+            &nbsp;
+            <a href="tel:+4904919293713">+49 (0) 491 9293713</a>
+          </p>
+          <p>
+            <MdEmail
+              className={styles.icon}
+              size={20}
+              style={{ verticalAlign: "middle" }}
+            />
+            &nbsp;
+            <a href="mailto:kontakt@wagemann-schweiss-service.de">
+              kontakt@wagemann-schweiss-service.de
+            </a>
+          </p>
+          <h2 className={styles.subtitle}>Registereintrag:</h2>
+          <p className={styles.paragraph}>
+            Eintragung im Handelsregister.
+            <br />
+            Registergericht: Amtsgericht Aurich
+            <br />
+            Registernummer: HRB 204232
+          </p>
+          {/* <h2 className={styles.subtitle}>Umsatzsteuer:</h2> */}
+          <p className={styles.paragraph}>
+            Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz:
+            <br />
+            DE 123456789
+          </p>
+          <h2 className={styles.subtitle}>
+            Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:
+          </h2>
+          <p className={styles.paragraph}>
+            Helga Wagemann <br />
+            Sägemühlenstraße 89
+            <br />
+            26789 Leer (Ostfriesland)
+          </p>
+          <h2 className={styles.subtitle}>Streitschlichtung</h2>
+          <p className={styles.paragraph}>
+            Die Europäische Kommission stellt eine Plattform zur
+            Online-Streitbeilegung (OS) bereit:{" "}
+            <a
+              href="https://ec.europa.eu/consumers/odr"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              https://ec.europa.eu/consumers/odr
+            </a>
+            .<br />
+            Unsere E-Mail-Adresse finden Sie oben im Impressum.
+            <br />
+            Wir sind nicht bereit oder verpflichtet, an
+            Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle
+            teilzunehmen.
+          </p>
+          <h3 className={styles.subtitle}>Haftung für Inhalte</h3>
+          <p className={styles.paragraph}>
+            Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte
+            auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach
+            §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht
+            verpflichtet, übermittelte oder gespeicherte fremde Informationen zu
+            überwachen oder nach Umständen zu forschen, die auf eine
+            rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung
+            oder Sperrung der Nutzung von Informationen nach den allgemeinen
+            Gesetzen bleiben hiervon unberührt.
+          </p>
+          <h3 className={styles.subtitle}>Haftung für Links</h3>
+          <p className={styles.paragraph}>
+            Unser Angebot enthält Links zu externen Websites Dritter, auf deren
+            Inhalte wir keinen Einfluss haben. Deshalb können wir für diese
+            fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der
+            verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber
+            der Seiten verantwortlich. Die verlinkten Seiten wurden zum
+            Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft.
+            Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht
+            erkennbar.
+          </p>
+          <h3 className={styles.subtitle}>Urheberrecht</h3>
+          <p className={styles.paragraph}>
+            Die durch die Seitenbetreiber erstellten Inhalte und Werke auf
+            diesen Seiten unterliegen dem deutschen Urheberrecht. Die
+            Vervielfältigung, Bearbeitung, Verbreitung und jede Art der
+            Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der
+            schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.
+            Downloads und Kopien dieser Seite sind nur für den privaten, nicht
+            kommerziellen Gebrauch gestattet.
+          </p>{" "}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
